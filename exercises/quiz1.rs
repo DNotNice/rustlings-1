e2546ea575b0a8d75ac814554f912d87e47cb440
Devansh Pandey <96971065+DNotNice@users.noreply.github.com>
2024-07-27 10:49:46 +0000

    rust-lings

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
-// Don't modify this function!
+ fn calculate_price_of_apples( quantity : i32 ) -> i32{
+    quantity * if quantity > 40 {1} else {2} 
+ }
+ // Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
